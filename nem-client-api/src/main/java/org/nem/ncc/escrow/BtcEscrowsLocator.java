0e3d3d96a5fcb836e5c2ae3efc37eb4bc0871082
Allan Kenneth Ang <allkenang@gmail.com>
2016-09-12 15:18:52 +0800

    removed unused casts formatted code for clarity

--- nem-client-api/src/main/java/org/nem/ncc/escrow/BtcEscrowsLocator.java
+++ nem-client-api/src/main/java/org/nem/ncc/escrow/BtcEscrowsLocator.java
@@ -1,21 +1,39 @@
 package org.nem.ncc.escrow;
 
-import org.nem.ncc.connector.*;
-import org.nem.ncc.services.*;
-import org.nem.core.model.ncc.*;
-import org.nem.core.model.*;
-import com.sharedobjects.nis.*;
-import com.sharedobjects.broker.*;
-import com.sharedobjects.*;
-import java.util.*;
-import java.util.function.*;
-import org.apache.commons.lang3.tuple.*;
-import org.nem.ncc.model.*;
-import java.util.stream.*;
-import java.math.*;
-import com.sharedobjects.client.*;
-import java.time.*;
-import org.nem.ncc.trading.storage.*;
+import com.sharedobjects.BtcEscrowResponse;
+import com.sharedobjects.broker.NemRequestType;
+import com.sharedobjects.client.BtcEscrowStatus;
+import com.sharedobjects.nis.NisHelper;
+import com.sharedobjects.nis.NisTransactionHistoryHelper;
+import com.sharedobjects.nis.PrimaryNisConnector;
+
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+import org.nem.core.model.Account;
+import org.nem.core.model.Address;
+import org.nem.core.model.TransferTransaction;
+import org.nem.core.model.ncc.TransactionMetaDataPair;
+import org.nem.ncc.connector.PrimaryBrokerConnector;
+import org.nem.ncc.model.BtcEscrowAccount;
+import org.nem.ncc.model.TradeAddress;
+import org.nem.ncc.model.TradeInstrument;
+import org.nem.ncc.services.SecureRequestMapper;
+import org.nem.ncc.services.TradeInstrumentsProvider;
+import org.nem.ncc.services.TradingStorageServices;
+import org.nem.ncc.trading.storage.DiscoveredAddress;
+import org.nem.ncc.trading.storage.DiscoveredEntity;
+import org.nem.ncc.trading.storage.TradingStorage;
+import org.nem.ncc.trading.storage.TradingStorageName;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class BtcEscrowsLocator
 {
@@ -38,42 +56,50 @@ public class BtcEscrowsLocator
     public Collection<BtcEscrowAccount> getEscrowBalances(final TradingStorageName name) {
         final TradingStorage tradingStorage = this.tradingStorageServices.get(name);
         final Pair<Collection<DiscoveredAddress>, Integer> discoveredAddresses = this.discoverAddresses(tradingStorage);
-        tradingStorage.addBtcEscrowAddresses((Collection<DiscoveredAddress>)discoveredAddresses.getLeft());
+        tradingStorage.addBtcEscrowAddresses(discoveredAddresses.getLeft());
         final Collection<DiscoveredAddress> fullList = tradingStorage.getBtcEscrowAddresses();
         final Collection<BtcEscrowAccount> escrowBalances = this.getBalances(this.secureRequestMapper.toTradingAccount(tradingStorage), fullList);
-        escrowBalances.addAll(this.getEmptyAccounts((int)discoveredAddresses.getRight(), tradingStorage.getTradingAccountAddress()));
+        escrowBalances.addAll(this.getEmptyAccounts(discoveredAddresses.getRight(), tradingStorage.getTradingAccountAddress()));
         return escrowBalances;
     }
     
     public List<TransactionMetaDataPair> getBtcEscrowRequests(final Address tradingAddress, final Collection<Address> brokerAddresses, final Long lastScannedBtcTxId) {
-        return (List<TransactionMetaDataPair>)NisTransactionHistoryHelper.getAllOutgoingTransactions(this.nisConnector, tradingAddress, (Long)null, txPair -> this.isBtcEscrowRequest(txPair, new HashSet<Address>(brokerAddresses)), txPair -> ((TransactionMetaData)txPair.getMetaData()).getId().equals(lastScannedBtcTxId));
+        return NisTransactionHistoryHelper.getAllOutgoingTransactions(this.nisConnector,
+                                                                    tradingAddress,
+                                                                    null,
+                                                                    txPair -> this.isBtcEscrowRequest(txPair, new HashSet<>(brokerAddresses)),
+                                                                    txPair -> (txPair.getMetaData()).getId().equals(lastScannedBtcTxId));
     }
     
     public List<TransactionMetaDataPair> getBtcEscrowResponses(final Account tradingAccount, final Collection<Address> brokerAddresses, final Long lastScannedBtcTxId) {
-        return (List<TransactionMetaDataPair>)NisTransactionHistoryHelper.getAllIncomingTransactions(this.nisConnector, tradingAccount.getAddress(), (Long)null, txPair -> this.isBtcEscrowResponse(tradingAccount, txPair, new HashSet<Address>(brokerAddresses)), txPair -> ((TransactionMetaData)txPair.getMetaData()).getId().equals(lastScannedBtcTxId));
+        return NisTransactionHistoryHelper.getAllIncomingTransactions(this.nisConnector,
+                                                                    tradingAccount.getAddress(),
+                                                                    null,
+                                                                    txPair -> this.isBtcEscrowResponse(tradingAccount, txPair, new HashSet<>(brokerAddresses)),
+                                                                    txPair -> (txPair.getMetaData()).getId().equals(lastScannedBtcTxId));
     }
     
     public boolean isBtcEscrowRequest(final TransactionMetaDataPair trPair, final Set<Address> recipients) {
-        if (((Transaction)trPair.getEntity()).getType() != 257) {
+        if ((trPair.getEntity()).getType() != 257) {
             return false;
         }
         final TransferTransaction tr = (TransferTransaction)trPair.getEntity();
         if (!recipients.contains(tr.getRecipient().getAddress())) {
             return false;
         }
-        final NemRequestType requestType = NisHelper.getInstance().getNemRequestType(((TransactionMetaData)trPair.getMetaData()).getId(), tr, tr.getRecipient());
+        final NemRequestType requestType = NisHelper.getInstance().getNemRequestType((trPair.getMetaData()).getId(), tr, tr.getRecipient());
         return NemRequestType.BTC_KEY_GENERATION == requestType;
     }
     
     public boolean isBtcEscrowResponse(final Account recipient, final TransactionMetaDataPair trPair, final Set<Address> senders) {
-        if (((Transaction)trPair.getEntity()).getType() != 257) {
+        if ((trPair.getEntity()).getType() != 257) {
             return false;
         }
         final TransferTransaction tr = (TransferTransaction)trPair.getEntity();
         if (!senders.contains(tr.getSigner().getAddress())) {
             return false;
         }
-        final String message = NisHelper.getInstance().decodeMessage(((TransactionMetaData)trPair.getMetaData()).getId(), tr, recipient);
+        final String message = NisHelper.getInstance().decodeMessage((trPair.getMetaData()).getId(), tr, recipient);
         return BtcEscrowResponse.isValid(message);
     }
     
@@ -83,15 +109,26 @@ public class BtcEscrowsLocator
         final Collection<Address> brokerAddresses = Arrays.asList(this.brokerConnector.requestBrokerAccount().getAddress());
         final List<TransactionMetaDataPair> requests = this.getBtcEscrowRequests(tradingAccount.getAddress(), brokerAddresses, tradingStorage.getLastScannedTxId(btc.getCode()));
         final List<TransactionMetaDataPair> responses = this.getBtcEscrowResponses(tradingAccount, brokerAddresses, tradingStorage.getLastScannedTxId(btc.getCode()));
-        final List<TradeAddress> processedAddresses = tradingStorage.getBtcEscrowAddresses().stream().map((Function<? super DiscoveredAddress, TradeAddress>)DiscoveredAddress::getAddress).collect(Collectors.toList());
+
+        final List<TradeAddress> processedAddresses = tradingStorage.getBtcEscrowAddresses().stream()
+                                                                                            .map(DiscoveredAddress::getAddress)
+                                                                                            .collect(Collectors.toList());
+
         final List<ImmutablePair<TransactionMetaDataPair, DiscoveredAddress>> matched = this.btcEscrowsServices.match(tradingAccount, requests, responses, processedAddresses);
-        this.btcEscrowsServices.getLastToScanTx((List<? extends Pair<TransactionMetaDataPair, DiscoveredAddress>>)matched).ifPresent(txId -> tradingStorage.setLastScannedTxId(btc.getCode(), txId));
-        final List<DiscoveredAddress> resolvedAddresses = matched.stream().filter(pair -> pair.getRight() != null).map(Pair::getRight).collect(Collectors.toList());
-        return (Pair<Collection<DiscoveredAddress>, Integer>)new ImmutablePair((Object)resolvedAddresses, (Object)(matched.size() - resolvedAddresses.size()));
+
+        this.btcEscrowsServices.getLastToScanTx(matched)
+                                .ifPresent(txId -> tradingStorage.setLastScannedTxId(btc.getCode(), txId));
+
+        final List<DiscoveredAddress> resolvedAddresses = matched.stream()
+                                                                .filter(pair -> pair.getRight() != null)
+                                                                .map(Pair::getRight).collect(Collectors.toList());
+
+        return (Pair<Collection<DiscoveredAddress>, Integer>)new ImmutablePair(resolvedAddresses, (matched.size() - resolvedAddresses.size()));
     }
     
     private Collection<BtcEscrowAccount> getEmptyAccounts(final int count, final Address tradingAccountAddress) {
-        return IntStream.range(0, count).mapToObj(index -> this.getEmptyAccount(tradingAccountAddress)).collect(Collectors.toList());
+        return IntStream.range(0, count).mapToObj(index -> this.getEmptyAccount(tradingAccountAddress))
+                                        .collect(Collectors.toList());
     }
     
     private BtcEscrowAccount getEmptyAccount(final Address tradingAccountAddress) {
@@ -99,8 +136,16 @@ public class BtcEscrowsLocator
     }
     
     private Collection<BtcEscrowAccount> getBalances(final Account account, final Collection<DiscoveredAddress> discoveredAddresses) {
-        final Collection<BtcEscrowAccount> balances = this.brokerConnector.getBtcEscrowBalances(account, (Collection<TradeAddress>)discoveredAddresses.stream().map((Function<? super DiscoveredAddress, TradeAddress>)DiscoveredAddress::getAddress).collect(Collectors.toList()));
-        balances.stream().forEach(escrow -> escrow.setCreated(discoveredAddresses.stream().filter(address -> address.getAddress().equals(escrow.getAddress())).map(DiscoveredEntity::getCreated).findFirst().orElse(Instant.ofEpochMilli(0L))));
+        final Collection<BtcEscrowAccount> balances = this.brokerConnector.getBtcEscrowBalances(account, discoveredAddresses.stream()
+                                                                                                                            .map(DiscoveredAddress::getAddress)
+                                                                                                                            .collect(Collectors.toList()));
+
+        balances.stream()
+                .forEach(escrow -> escrow.setCreated(discoveredAddresses.stream()
+                                                                        .filter(address -> address.getAddress().equals(escrow.getAddress()))
+                                                                        .map(DiscoveredEntity::getCreated)
+                                                                        .findFirst()
+                                                                        .orElse(Instant.ofEpochMilli(0L))));
         return balances;
     }
 }
