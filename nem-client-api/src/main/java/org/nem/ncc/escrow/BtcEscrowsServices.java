464d57c86ac83c33256ade3f0bc8c218c64a0295
Allan Kenneth Ang <allkenang@gmail.com>
2016-09-16 13:45:01 +0800

    formatted codes

--- nem-client-api/src/main/java/org/nem/ncc/escrow/BtcEscrowsServices.java
+++ nem-client-api/src/main/java/org/nem/ncc/escrow/BtcEscrowsServices.java
@@ -16,26 +16,40 @@ import java.util.function.*;
 public class BtcEscrowsServices
 {
     public List<ImmutablePair<TransactionMetaDataPair, DiscoveredAddress>> match(final Account recipient, final List<TransactionMetaDataPair> requestTxs, final List<TransactionMetaDataPair> responseTxs, final List<TradeAddress> processedAddresses) {
-        final List<DiscoveredAddress> publishedAddresses = responseTxs.stream().map(responseTx -> this.responseToAddress(recipient, responseTx)).filter(discoveredAddress -> !processedAddresses.contains(discoveredAddress.getAddress())).collect(Collectors.toList());
-        requestTxs.sort((t1, t2) -> ((Transaction)t1.getEntity()).getTimeStamp().compareTo(((Transaction)t2.getEntity()).getTimeStamp()));
+        final List<DiscoveredAddress> publishedAddresses = responseTxs.stream()
+                                                                        .map(responseTx -> this.responseToAddress(recipient, responseTx))
+                                                                        .filter(discoveredAddress -> !processedAddresses.contains(discoveredAddress.getAddress()))
+                                                                        .collect(Collectors.toList());
+
+        requestTxs.sort((t1, t2) -> t1.getEntity().getTimeStamp().compareTo(t2.getEntity().getTimeStamp()));
         final int responsesSize = publishedAddresses.size();
         final ImmutablePair<TransactionMetaDataPair, DiscoveredAddress> immutablePair = null;
         final int n = 0;
         final List<DiscoveredAddress> list = new ArrayList<>();
-        return IntStream.range(0, requestTxs.size()).mapToObj(i -> {
-            new ImmutablePair((Object)requestTxs.get(i), (Object)((i >= n) ? null : list.get(i)));
-            return immutablePair;
-        }).collect(Collectors.toList());
+
+        return IntStream.range(0, requestTxs.size())
+                        .mapToObj(i -> { new ImmutablePair(requestTxs.get(i), ((i >= n) ? null : list.get(i)));
+                                            return immutablePair;
+                        })
+                        .collect(Collectors.toList());
     }
     
     private DiscoveredAddress responseToAddress(final Account recipient, final TransactionMetaDataPair response) {
-        final String messageData = NisHelper.getInstance().decodeMessage(((TransactionMetaData)response.getMetaData()).getId(), (TransferTransaction)response.getEntity(), recipient);
+        final String messageData = NisHelper.getInstance().decodeMessage(response.getMetaData().getId(), (TransferTransaction)response.getEntity(), recipient);
         final BtcEscrowResponse escrowResponse = new BtcEscrowResponse(BtcEscrowResponse.getAddress(messageData));
-        return new DiscoveredAddress(new TradeAddress(escrowResponse.getBtcAddress()), Instant.ofEpochMilli(UnixTime.fromTimeInstant(((Transaction)response.getEntity()).getTimeStamp()).getMillis()));
+        return new DiscoveredAddress(new TradeAddress(escrowResponse.getBtcAddress()),
+                                                        Instant.ofEpochMilli(UnixTime.fromTimeInstant(response.getEntity().getTimeStamp()).getMillis()));
     }
     
     public Optional<Long> getLastToScanTx(final List<? extends Pair<TransactionMetaDataPair, DiscoveredAddress>> matchedRequests) {
-        final Optional<Long> lastUnresolvedRequestId = matchedRequests.stream().filter(pair -> pair.getRight() == null).reduce((a, b) -> b).map(pair -> ((TransactionMetaData)((TransactionMetaDataPair)pair.getLeft()).getMetaData()).getId() - 1L);
-        return lastUnresolvedRequestId.map(Optional::of).orElse(matchedRequests.stream().reduce((a, b) -> b).map(pair -> ((TransactionMetaData)((TransactionMetaDataPair)pair.getLeft()).getMetaData()).getId()));
+        final Optional<Long> lastUnresolvedRequestId = matchedRequests.stream()
+                                                                    .filter(pair -> pair.getRight() == null)
+                                                                    .reduce((a, b) -> b)
+                                                                    .map(pair -> (pair.getLeft().getMetaData()).getId() - 1L);
+
+        return lastUnresolvedRequestId.map(Optional::of)
+                                        .orElse(matchedRequests.stream()
+                                                                .reduce((a, b) -> b)
+                                                                .map(pair -> (pair.getLeft().getMetaData()).getId()));
     }
 }
